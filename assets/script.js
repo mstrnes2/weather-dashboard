2fbba354d1c7bd8ac738b80851404352db6766bc
Molly Starnes <mstrnes2@gmail.com>
2023-08-23 17:49:38 -0500

    fixed bugs for date and fav button

--- assets/script.js
+++ assets/script.js
@@ -5,21 +5,23 @@ var temp = document.querySelector(".temp");
 var humidity = document.querySelector(".humidity");
 var windSpeed = document.querySelector(".wind-speed");
 var searchInput = document.querySelector(".location");
-var weatherApiRootUrl = 'https://api.openweathermap.org';
+var weatherApiRootUrl = "https://api.openweathermap.org";
 var apiKey = "7ab439372a6b7834b1058543aced3bee";
-var weatherResults = document.querySelector(".hide");
+var weatherResults = document.getElementById("weather-results");
 var searchCityBtn = document.querySelector(".btn");
+var forecastContainer = document.getElementById("forecast-container");
 
 updateFavoriteCitiesList();
 
-searchCityBtn.addEventListener("click", function(event) {
+searchCityBtn.addEventListener("click", function (event) {
   event.preventDefault();
   var cityName = document.querySelector(".location").value.trim();
 
-
   var favoriteCities = JSON.parse(localStorage.getItem("favoriteCities")) || [];
 
-  favoriteCities.push(cityName);
+  favoriteCities.unshift(cityName);
+
+  favoriteCities.slice(5);
 
   localStorage.setItem("favoriteCities", JSON.stringify(favoriteCities));
 
@@ -29,8 +31,8 @@ searchCityBtn.addEventListener("click", function(event) {
 function updateFavoriteCitiesList() {
   var favoriteCities = JSON.parse(localStorage.getItem("favoriteCities")) || [];
   var listGroup = document.querySelector(".list-group");
-  
-  listGroup.innerHTML = ""; 
+
+  listGroup.innerHTML = "";
 
   for (var i = 0; i < 5; i++) {
     const favCityEl = document.createElement("button");
@@ -38,36 +40,43 @@ function updateFavoriteCitiesList() {
     favCityEl.textContent = favoriteCities[i];
     listGroup.append(favCityEl);
   }
+  searchCityListeners();
 }
 
-var favCityBtn = document.querySelectorAll(".list-group-item");
-
-for (var i = 0; i < favCityBtn.length; i++) {
-  favCityBtn[i].addEventListener("click", function(event) {
-    var cityName = event.target.textContent;
-    fetchCoords(cityName);
-    weatherResults.classList.remove("hide");
-  });
+function searchCityListeners() {
+  var favCityBtn = document.querySelectorAll(".list-group-item");
+  for (var i = 0; i < favCityBtn.length; i++) {
+    favCityBtn[i].addEventListener("click", function (event) {
+      var cityName = event.target.textContent;
+      fetchCoords(cityName);
+      weatherResults.classList.remove("hide");
+      forecastContainer.classList.remove("hide");
+    });
+  }
 }
 
+searchCityListeners();
 
-function displayCurrentWeather(data){
-    cityName.textContent = data.name;
-    date.textContent = dayjs.unix(data.dt).format("MM/DD/YYYY");
-    weatherIcon.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-    temp.textContent = `${data.main.temp}° F`;
-    humidity.textContent = `Humidity: ${data.main.humidity} %`;
-    windSpeed.textContent = `Wind Speed: ${data.wind.speed} MPH`;
+function displayCurrentWeather(data) {
+  cityName.textContent = data.name;
+  date.textContent = dayjs.unix(data.dt).format("MM/DD/YYYY");
+  weatherIcon.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  temp.textContent = `${data.main.temp}° F`;
+  humidity.textContent = `Humidity: ${data.main.humidity} %`;
+  windSpeed.textContent = `Wind Speed: ${data.wind.speed} MPH`;
 }
 
 function displayForecast(data) {
-
-  for (let i = 0; i < data.list.length; i++) {
-    const forecastDate = dayjs.unix(data.list[i].dt).format("MM/DD/YYYY");
-    const forecastIcon = data.list[i].weather[0].icon;
-    const forecastTemp = `${data.list[i].main.temp}° F`;
-    const forecastHumidity = `Humidity: ${data.list[i].main.humidity} %`;
-    const forecastWind = `Wind Speed: ${data.list[i].wind.speed} MPH`
+  let weatherData = [];
+  for (let i = 3; i < data.list.length; i += 8) {
+    weatherData.push(data.list[i]);
+  }
+  for (let i = 0; i < weatherData.length; i++) {
+    const forecastDate = dayjs.unix(weatherData[i].dt).format("MM/DD/YYYY");
+    const forecastIcon = weatherData[i].weather[0].icon;
+    const forecastTemp = `${weatherData[i].main.temp}° F`;
+    const forecastHumidity = `Humidity: ${weatherData[i].main.humidity} %`;
+    const forecastWind = `Wind Speed: ${weatherData[i].wind.speed} MPH`;
 
     const iconUrl = `https://openweathermap.org/img/w/${forecastIcon}.png`;
 
@@ -77,75 +86,75 @@ function displayForecast(data) {
     forecast.querySelector(".forecast-temp").textContent = forecastTemp;
     forecast.querySelector(".forecast-humidity").textContent = forecastHumidity;
     forecast.querySelector(".forecast-wind").textContent = forecastWind;
+  }
 }
 
-}
-
-document.querySelector(".btn").addEventListener("click", handleSearchFormSubmit)
+document
+  .querySelector(".btn")
+  .addEventListener("click", handleSearchFormSubmit);
 
 function handleSearchFormSubmit(e) {
+  weatherResults.classList.remove("hide");
+  forecastContainer.classList.remove("hide");
+  // Don't continue if there is nothing in the search form
+  if (!searchInput.value) {
+    return;
+  }
 
-    weatherResults.classList.remove("hide");
-    // Don't continue if there is nothing in the search form
-    if (!searchInput.value) {
-      return
-    }
-  
-    e.preventDefault()
-    var search = searchInput.value.trim()
-    fetchCoords(search)
-    searchInput.value = ''
-  }
-  function fetchCoords(search) {
-    var apiUrl = `${weatherApiRootUrl}/geo/1.0/direct?q=${search}&limit=1&appid=${apiKey}`
-  
-    fetch(apiUrl)
-      .then(function (res) {
-        return res.json()
-      })
-      .then(function (data) {
-        if (!data[0]) {
-          alert('Location not found')
-        } else {
-          //console.log(data[0])
-          //appendToHistory(search)
-          fetchWeather(data[0])
-        }
-      })
-      .catch(function (err) {
-        console.error(err)
-      })
-  }
+  e.preventDefault();
+  var search = searchInput.value.trim();
+  fetchCoords(search);
+  searchInput.value = "";
+}
+function fetchCoords(search) {
+  var apiUrl = `${weatherApiRootUrl}/geo/1.0/direct?q=${search}&limit=1&appid=${apiKey}`;
+
+  fetch(apiUrl)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      if (!data[0]) {
+        alert("Location not found");
+      } else {
+        //console.log(data[0])
+        //appendToHistory(search)
+        fetchWeather(data[0]);
+      }
+    })
+    .catch(function (err) {
+      console.error(err);
+    });
+}
 
 function fetchWeather(location) {
-    var { lat, lon, name } = location
-  
-    //   get the current waether now, uses city name
-    var apiUrlWeather = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&q=${name}&units=imperial`
-  
-    //   get the forecast, uses lat/lon
-    var apiUrlForecast = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
-  
-    fetch(apiUrlWeather)
-      .then(function (res) {
-        return res.json()
-      })
-      .then(function (data) {
-        //console.log(data)
-        displayCurrentWeather(data)
-      })
-      .then(function () {
-        fetch(apiUrlForecast)
-          .then(function (res) {
-            return res.json()
-          })
-          .then(function (data) {
-            //console.log(data)
-            displayForecast(data)
-          })
-      })
-      .catch(function (err) {
-        console.error(err)
-      })
-  }
+  var { lat, lon, name } = location;
 
+  //   get the current waether now, uses city name
+  var apiUrlWeather = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&q=${name}&units=imperial`;
+
+  //   get the forecast, uses lat/lon
+  var apiUrlForecast = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+
+  fetch(apiUrlWeather)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      //console.log(data)
+      displayCurrentWeather(data);
+    })
+    .then(function () {
+      fetch(apiUrlForecast)
+        .then(function (res) {
+          return res.json();
+        })
+        .then(function (data) {
+          //console.log(data)
+          displayForecast(data);
+        });
+    })
+    .catch(function (err) {
+      console.error(err);
+    });
+}
